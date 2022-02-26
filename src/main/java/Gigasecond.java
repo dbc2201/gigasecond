684d3e443a68cbafed2c66130b755c7ec57abd28
dbc2201 <divyanshb22@gmail.com>
2022-02-26 21:34:56 +0530

    create a constant with the value of one giga second, calculate the value of one giga second with a method

--- src/main/java/Gigasecond.java
+++ src/main/java/Gigasecond.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 public class Gigasecond {
 
     private LocalDateTime moment;
+    private final long ONE_GIGA_SECOND = calculateOneGigaSecond();
 
     public Gigasecond(LocalDate moment) {
         this.moment = moment.atStartOfDay();
@@ -15,5 +16,9 @@ public class Gigasecond {
 
     public LocalDateTime getDateTime() {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    private long calculateOneGigaSecond() {
+        return Double.valueOf(Math.pow(10.0, 9.0)).longValue();
     }
 }
